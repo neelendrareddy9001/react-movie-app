6bb44039e131acbe3af58d54358c8462fb7a284b
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-11-16 22:55:37 +0100

    movie listing from api and styles of it

--- src/components/MovieListing/MovieListing.js
+++ src/components/MovieListing/MovieListing.js
@@ -1,11 +1,30 @@
 import React from "react";
 import { useSelector } from "react-redux/es/hooks/useSelector";
 import { getAllMovies } from "../../features/Movies/movieSlice";
+import MovieCard from "../MovieCard/MovieCard";
 
 const MovieListing = () => {
   const movies = useSelector(getAllMovies);
+  let renderMovies = "";
+  renderMovies =
+    movies.Response === "True" ? (
+      movies.Search.map((movie, index) => (
+        <MovieCard key={index} data={movie} />
+      ))
+    ) : (
+      <div className="movies-error">
+        <h3>{movies.Error}</h3>
+      </div>
+    );
   console.log(movies);
-  return <div>MovieListing</div>;
+  return (
+    <div className="movie-wrapper">
+      <div className="movie-list">
+        <h2>Movies</h2>
+        <div className="movie-container"></div>
+      </div>
+    </div>
+  );
 };
 
 export default MovieListing;
