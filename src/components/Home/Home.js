7ee6696f69bdc6656c60dd06a04f5a9b80fb1801
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-11-22 23:02:00 +0100

    updated

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,18 +1,29 @@
 import React, { useEffect } from "react";
 import MovieListing from "../MovieListing/MovieListing";
-
+import movieApi from "../../common/apis/movieApi";
 import { useDispatch } from "react-redux";
 import {
   fetchAsyncMovies,
   fetchAsyncShows,
 } from "../../features/Movies/movieSlice";
+import { APIKEY } from "../../common/apis/MovieApiKeys";
 
 const Home = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchAsyncMovies());
-    dispatch(fetchAsyncShows());
-  }, [dispatch]);
+  
+  }
+useEffect(() => {
+   const moviesText = "Harry";
+  const fetchMovies = async () => {
+    const response = await movieApi.get(`?apiKey=${APIKEY}&s=${moviesText}&type=movie`).catch((err) => {
+      console.log("Err", err)
+    });
+   
+    cnsole.log("The response from API", response)
+    // dispatch(fetchAsyncMovies());
+    // dispatch(fetchAsyncShows());
+  };
+  fetchMovies();
+  }, [])
   return (
     <div>
       <div className="banner-img"></div>
